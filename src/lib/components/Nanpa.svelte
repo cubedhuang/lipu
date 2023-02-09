ba0c34f584ab3ecc21822dc1b82b418a1a88b9ce
cubedhuang <99danielh@gmail.com>
2023-02-09 00:06:17 -0500

    mi pona e elen nanpa

--- src/lib/components/Nanpa.svelte
+++ src/lib/components/Nanpa.svelte
@@ -6,20 +6,20 @@
 	class="not-prose flex w-fit rounded-lg bg-gray-50 shadow border border-gray-200"
 >
 	<button
-		class="z-10 p-2 hover:bg-gray-100 font-pona focus-outline rounded-lg"
+		class="z-10 px-2 font-pona text-2xl focus-outline rounded-lg hover:bg-gray-100 transition-colors"
 		on:click={() => count--}
 	>
 		lili
 	</button>
 
 	<p
-		class="w-16 py-2 bg-white border-x border-gray-200 text-center font-bold tabular-nums"
+		class="w-16 py-1 grid place-items-center bg-white border-x border-gray-200 text-center font-bold tabular-nums"
 	>
 		{count}
 	</p>
 
 	<button
-		class="p-2 hover:bg-gray-100 font-pona focus-outline rounded-lg"
+		class="px-2 font-pona text-2xl focus-outline rounded-lg hover:bg-gray-100 transition-colors"
 		on:click={() => count++}
 	>
 		suli
