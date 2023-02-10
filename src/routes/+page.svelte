93bbb0989bd01151bb155112b306eb519d6f01e4
cubedhuang <99danielh@gmail.com>
2023-02-10 12:22:05 -0500

    mi suli e sitelen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,19 +17,19 @@
 
 <Meta title="lipu tan ijo Tani" description="ni li kulupu lipu tan ijo Tani!" />
 
-<div class="mt-16 mx-auto prose">
+<div class="mt-16 mx-auto prose prose-lg">
 	<h1>lipu tan ijo Tani!</h1>
 	<p>kulupu lipu ni la, mi sitelen e ijo sona mi.</p>
 </div>
 
-<ul class="mt-8 mb-20 mx-auto max-w-prose grid sm:grid-cols-2 gap-4">
+<ul class="mt-8 mb-20 mx-auto max-w-prose text-lg grid sm:grid-cols-2 gap-4">
 	{#each data.lipu as lipu}
 		<a href="/lipu/{lipu.slug}" class="rounded-lg focus-outline">
 			<li
 				class="h-full p-6 rounded-lg bg-gray-50 shadow border border-gray-200
 					hover:shadow-lg hover:scale-[1.02] transition"
 			>
-				<article class="prose">
+				<article class="prose prose-lg">
 					<h2 class="mb-1">{lipu.title}</h2>
 
 					<p class="my-1 text-sm text-gray-500">
--- src/routes/lipu/[slug]/+page.svelte
+++ src/routes/lipu/[slug]/+page.svelte
@@ -4,10 +4,10 @@
 	export let data: PageData;
 </script>
 
-<nav class="mt-8 mx-auto max-w-prose">
+<nav class="mt-8 mx-auto max-w-prose text-lg">
 	<a href="/" class="link">lipu ale</a>
 </nav>
 
-<main class="mt-8 mb-20 mx-auto prose prose-blue prose-a:link">
+<main class="mt-8 mb-20 mx-auto prose prose-lg prose-blue prose-a:link">
 	<svelte:component this={data.component} />
 </main>
