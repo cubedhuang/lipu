25da5c75cb7ad610506a612a02d922656b0d8938
cubedhuang <99danielh@gmail.com>
2023-02-25 12:38:15 -0500

    mi pona e kule <blockquote>

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -11,7 +11,7 @@
 <main
 	class="mt-8 mb-20 mx-auto
 		prose prose-blue prose-a:link
-		prose-blockquote:font-normal prose-blockquote:not-italic
+		prose-blockquote:font-normal prose-blockquote:text-inherit prose-blockquote:not-italic
 		prose-p:before:content-none prose-p:after:content-none"
 >
 	<svelte:component this={data.component} />
