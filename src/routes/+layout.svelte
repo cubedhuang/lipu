7b5d1f8e1eaa99825dec337d1c5fd89113586687
cubedhuang <99danielh@gmail.com>
2023-11-02 18:29:52 -0400

    mi ante e tan sitelen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,6 @@
 	import '../app.pcss';
 </script>
 
-<svelte:head>
-	<link rel="preconnect" href="https://rsms.me/" />
-	<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-</svelte:head>
-
 <div class="px-8">
 	<slot />
 </div>
