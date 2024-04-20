bb7bdc33e633d92f674214f22c7eaa019c0ece64
cubedhuang <99danielh@gmail.com>
2024-04-19 23:45:56 -0400

    mi kule e toki

---
+++ src/lib/highlight-pona.js
@@ -0,0 +1,33 @@
+/**
+ * @type {import("highlight.js").LanguageFn}
+ */
+export function highlightPona(hljs) {
+	console.log('highlight-pona.js');
+	return {
+		keywords: {
+			keyword: 'lukin pana kama ni',
+			type: 'nasin toki nanpa ken',
+			literal: ['lon', 'lon ala'],
+			separator: 'la li e pi o anu en'
+			// number: 'ale mute luka tu wan ala'
+		},
+		contains: [
+			hljs.C_LINE_COMMENT_MODE,
+			hljs.APOS_STRING_MODE,
+			hljs.QUOTE_STRING_MODE,
+			hljs.BINARY_NUMBER_MODE,
+			hljs.C_NUMBER_MODE,
+			{
+				scope: 'number',
+				// 'ale mute luka tu wan ala' only after 'nanpa'
+				begin: /\b(?<=nanpa)\s+((ale|mute|luka|tu|wan|ala)\s+)+/,
+				relevance: 0
+			},
+			{
+				scope: 'title',
+				begin: /\b[A-Z]\w*/,
+				relevance: 0
+			}
+		]
+	};
+}
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,21 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import hljs from 'highlight.js/lib/core';
 import { mdsvex } from 'mdsvex';
 import slug from 'rehype-slug';
 import containers from 'remark-containers';
 import gfm from 'remark-gfm';
 
+import { highlightPona } from './src/lib/highlight-pona.js';
+
+hljs.registerLanguage('pona', highlightPona);
+
 const mdsvexPreprocess = mdsvex({
+	highlight: {
+		highlighter: (code, lang) => {
+			return `<pre class="hljs"><code class="language-${lang}">${hljs.highlightAuto(code, [lang]).value}</code></pre>`;
+		}
+	},
 	layout: {
 		_: './src/lib/components/Meta.svelte'
 	},
